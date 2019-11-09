7a5fb3571cb54499c0db6979b05aa497fa4954b0
Safa n <safa.nazir@ontariotechu.net>
2019-11-09 11:22:08 -0500

    Basic scene setup, player 1 can move

---
+++ Assets/_Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed = 2;
+    Vector3 rotation = new Vector3();
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        transform.position += (Vector3.forward * speed) * y * Time.deltaTime;
+        transform.position += (Vector3.right * speed) * x * Time.deltaTime;
+    }
+}
