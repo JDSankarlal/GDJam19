2ba67eba611c80c7ac3880bb002a751f3e208cd5
Gil <gil.robern@ontariotechu.net>
2019-11-09 13:47:48 -0500

    TImer
    Uses a progress bar to show how long they can hold the item for

---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Image bar;
+    public float shotClockTime;
+    //private float shotClock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bar.GetComponent<Image>();
+        //shotClock = shotClockTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //shotClock -= Time.deltaTime;
+
+        //bar.fillAmount -= (shotClockTime / 100);
+        bar.fillAmount -= Time.deltaTime / shotClockTime;
+
+        Debug.Log(bar.fillAmount);
+        //Debug.Log(shotClock);
+    }
+
+}
