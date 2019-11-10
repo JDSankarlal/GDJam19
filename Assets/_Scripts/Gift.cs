fad7be359ea4a1177530a0450d6de61073ceb1d9
Josh <joshua.sankarlal@ontariotechu.net>
2019-11-10 12:11:56 -0500

    Delete Scripts, Collisions in Gift

--- Assets/_Scripts/Gift.cs
+++ Assets/_Scripts/Gift.cs
@@ -15,30 +15,42 @@ public class Gift : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-          if (gameObject.transform.parent.tag=="Player") // And the parent is a Player
-            {
-                if (this.gameObject.transform.parent.name == "Player1") //Check which player
-                    ThrowGift(""); //No string because Player1 uses "Horizontal" input
-                if (this.gameObject.transform.parent.name == "Player2")
-                    ThrowGift("2");
-                if (this.gameObject.transform.parent.name == "Player3")
-                    ThrowGift("3");
-                if (this.gameObject.transform.parent.name == "Player4")
-                    ThrowGift("4");
-            }
-
-        }
-
-        void ThrowGift(string num)
+        if (gameObject.transform.parent.tag == "Player") // And the parent is a Player
         {
-            if (Input.GetButton("Throw" + num))
-            {
-                Debug.Log("Gift Throw Pressed");
-
-                GiftRb.AddForce(100, 0, 0);
-                gameObject.transform.parent = null; //Child moves out
-
-            }
+            if (this.gameObject.transform.parent.name == "Player1") //Check which player
+                ThrowGift(""); //No string because Player1 uses "Horizontal" input
+            if (this.gameObject.transform.parent.name == "Player2")
+                ThrowGift("2");
+            if (this.gameObject.transform.parent.name == "Player3")
+                ThrowGift("3");
+            if (this.gameObject.transform.parent.name == "Player4")
+                ThrowGift("4");
         }
+
+    }
+
+    void ThrowGift(string num)
+    {
+        if (Input.GetButton("Throw" + num))
+        {
+            Debug.Log("Gift Throw Pressed");
+
+            GiftRb.AddForce(gameObject.transform.forward * 100);
+            gameObject.transform.parent = null; //Child moves out
+
+        }
+    }
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("touched gift");
+            gameObject.transform.SetParent(collision.gameObject.transform);
+            //collision.gameObject.transform.position= new Vector3(collision.gameObject.transform.position.x,collision.gameObject.transform.position.y,collision.gameObject.transform.position.z);
+        }
+    }
 }
 
