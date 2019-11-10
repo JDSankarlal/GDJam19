ae13a68dd4e52693eba0baca1d41b8566572cc73
Josh <joshua.sankarlal@ontariotechu.net>
2019-11-10 18:40:02 -0500

    Revert "Particles on Box"
    This reverts commit 551eed7aa28f5e684dbe8daae636421a8853f6f9.

--- Assets/_Scripts/Gift.cs
+++ Assets/_Scripts/Gift.cs
@@ -6,14 +6,12 @@ public class Gift : MonoBehaviour
 {
     Rigidbody GiftRb;
     public bool resetTimer = false;
-    private bool isHeld = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
         GiftRb = gameObject.transform.GetComponent<Rigidbody>();
-        gameObject.GetComponent<ParticleSystem>().Stop();
-
-
     }
 
     // Update is called once per frame
@@ -31,23 +29,19 @@ public class Gift : MonoBehaviour
                 ThrowGift("4");
         }
 
-         isBeingHeld(isHeld);
-
     }
 
     void ThrowGift(string num)
     {
         if (Input.GetButton("Throw" + num))
         {
-            GiftRb.isKinematic = false;
-            isHeld = false;
             GameObject.Find("Player" + num).GetComponentInChildren<ParticleSystem>().Stop();
 
             Debug.Log("Gift Throw Pressed");
+
             GiftRb.AddForce(gameObject.transform.parent.forward * 1000);
             gameObject.transform.parent = null; //Child moves out
             gameObject.GetComponent<BoxCollider>().enabled = true;
-            gameObject.GetComponent<ParticleSystem>().Play();
 
         }
     }
@@ -58,32 +52,16 @@ public class Gift : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            
-            gameObject.transform.SetParent(collision.gameObject.transform);
-            gameObject.GetComponent<ParticleSystem>().Stop();
-            Debug.Log("touched gift");
-            isHeld = true;
             resetTimer = true;
-            //collision.gameObject.transform.position= new Vector3(collision.gameObject.transform.position.x,collision.gameObject.transform.position.y,collision.gameObject.transform.position.z);
-        }
-    }
-
-    void isBeingHeld(bool _isHeld)
-    {
-        if (_isHeld)
-        {
-            GiftRb.isKinematic = true;
+            collision.gameObject.GetComponentInChildren<ParticleSystem>().Play();
+            Debug.Log("touched gift");
             GiftRb.velocity = Vector3.zero;
             GiftRb.angularVelocity = Vector3.zero;  
-            gameObject.transform.localPosition = new Vector3(0,0,3);
+            gameObject.transform.SetParent(collision.gameObject.transform);
             gameObject.GetComponent<BoxCollider>().enabled = false;
-        }
-        
-    }
 
-    void LateUpdate()
-    {
-      
+            //collision.gameObject.transform.position= new Vector3(collision.gameObject.transform.position.x,collision.gameObject.transform.position.y,collision.gameObject.transform.position.z);
+        }
     }
 }
 
