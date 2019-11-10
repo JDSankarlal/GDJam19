38372bf8c69b0d6ba8b8a21e8302c894ddd769c1
Josh <joshua.sankarlal@ontariotechu.net>
2019-11-10 00:01:24 -0500

    It only gets worse

--- Assets/_Scripts/playerMovement.cs
+++ Assets/_Scripts/playerMovement.cs
@@ -8,21 +8,64 @@ public class playerMovement : MonoBehaviour
     public float acceleration = 5f;
     public int MaxSpeed = 25;
     Vector3 rotation = new Vector3();
-    private CharacterController cc1;
-    private CharacterController cc2;
-    private CharacterController cc3;
-    private CharacterController cc4;
+    private CharacterController cc;
     // Start is called before the first frame update
     void Start()
     {
-        cc1 = GameObject.Find("Player1").GetComponent<CharacterController>();
-        cc2 = GameObject.Find("Player2").GetComponent<CharacterController>();
-        cc3 = GameObject.Find("Player3").GetComponent<CharacterController>();
-        cc4 = GameObject.Find("Player4").GetComponent<CharacterController>();
+        cc = gameObject.GetComponent<CharacterController>();
+      
     }
 
     // Update is called once per frame
     void Update()
+    {
+      
+
+        if (this.gameObject.name == "Player1")
+        {
+           movePlayer("");
+        }
+
+        if (this.gameObject.name == "Player2")
+        {
+            movePlayer("2");
+        }
+
+        if (this.gameObject.name == "Player3")
+        {
+           movePlayer("3");
+        }
+
+        if (this.gameObject.name == "Player4")
+        {
+            movePlayer("4");
+        }
+
+        //if (move!=Vector3.zero)
+        //  transform.forward = move;
+
+    }
+
+
+    void movePlayer(string num)
+    {
+        if (Input.GetButton("Horizontal"+num) || Input.GetButton("Vertical"+num))
+            {
+              speed += Time.deltaTime * acceleration;
+                if (speed >= MaxSpeed)
+                    speed = MaxSpeed;
+            }
+
+            if (Input.GetButtonUp("Horizontal"+num) || Input.GetButtonUp("Vertical"+num))
+            {
+                speed = 10;
+            }
+
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"+num), 0, Input.GetAxis("Vertical"+num));
+            cc.Move(move * Time.deltaTime * speed);
+    }
+
+    void LateUpdate()
     {
         if (gameObject.tag=="Player")
         {
@@ -33,85 +76,7 @@ public class playerMovement : MonoBehaviour
         {
             GameObject.Find("Cube").transform.position =  new Vector3(0.0f, 0.5800052f, 0.0f);
         }
-
-        if (this.gameObject.name == "Player1")
-        {
-            if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
-            {
-                speed += Time.deltaTime * acceleration;
-                if (speed >= MaxSpeed)
-                    speed = MaxSpeed;
-            }
-
-            if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
-            {
-                speed = 10;
-            }
-
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            cc1.Move(move * Time.deltaTime * speed);
-        }
-
-        if (this.gameObject.name == "Player2")
-        {
-            if (Input.GetButton("Horizontal2") || Input.GetButton("Vertical2"))
-            {
-                speed += Time.deltaTime * acceleration;
-                if (speed >= MaxSpeed)
-                    speed = MaxSpeed;
-            }
-
-            if (Input.GetButtonUp("Horizontal2") || Input.GetButtonUp("Vertical2"))
-            {
-                speed = 10;
-            }
-
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal2"), 0, Input.GetAxis("Vertical2"));
-            cc2.Move(move * Time.deltaTime * speed);
-        }
-
-        if (this.gameObject.name == "Player3")
-        {
-            if (Input.GetButton("Horizontal3") || Input.GetButton("Vertical3"))
-            {
-                speed += Time.deltaTime * acceleration;
-                if (speed >= MaxSpeed)
-                    speed = MaxSpeed;
-            }
-
-            if (Input.GetButtonUp("Horizontal3") || Input.GetButtonUp("Vertical3"))
-            {
-                speed = 10;
-            }
-
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal3"), 0, Input.GetAxis("Vertical3"));
-            cc3.Move(move * Time.deltaTime * speed);
-        }
-
-        if (this.gameObject.name == "Player4")
-        {
-            if (Input.GetButton("Horizontal4") || Input.GetButton("Vertical4"))
-            {
-              speed += Time.deltaTime * acceleration;
-                if (speed >= MaxSpeed)
-                    speed = MaxSpeed;
-            }
-
-            if (Input.GetButtonUp("Horizontal4") || Input.GetButtonUp("Vertical4"))
-            {
-                speed = 10;
-            }
-
-            Vector3 move = new Vector3(Input.GetAxis("Horizontal4"), 0, Input.GetAxis("Vertical4"));
-            cc4.Move(move * Time.deltaTime * speed);
-        }
-
-        //if (move!=Vector3.zero)
-        //  transform.forward = move;
-
     }
-
-
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Gift")
